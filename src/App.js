124541b035f40b7a3d0bafa10c3fd9f90eed92af
Asraf Ansari <asraf3101aa@gmail.com>
2023-03-12 14:48:58 +0545

    Added todo list
    learned about props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './MyComponents/Header';
+import { Footer } from "./MyComponents/Footer";
+import { Todos } from "./MyComponents/Todos";
 function App() {
-  let tmp=345;
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get thsi job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the shop",
+      desc: "You need to go to the shop to get thsi job done2"
+    },
+    {
+      sno: 3,
+      title: "Go to the salon",
+      desc: "You need to go to the salon to get thsi job done3"
+    },
+
+  ]
+
   return (
     <>
-    <h3>My app</h3>
-    <p>My app works</p>
+      <Header title="My Todos List" searchbar={false} />
+      <Todos todos={todos}/>
+      <Footer />
     </>
+
   );
 }
 
