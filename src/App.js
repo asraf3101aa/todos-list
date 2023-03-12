67bb55664c77dd390c30d733fee34a397944f1bf
Asraf Ansari <asraf3101aa@gmail.com>
2023-03-12 12:54:56 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let tmp=345;
+  return (
+    <>
+    <h3>My app</h3>
+    <p>My app works</p>
+    </>
+  );
+}
+
+export default App;
